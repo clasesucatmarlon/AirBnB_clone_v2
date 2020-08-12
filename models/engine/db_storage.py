43aa7cc0efc5d5f09c1054b0f0d7b4aa2d34dd50
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-12 18:47:18 -0500

    Added dictionary for class and function all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -18,6 +18,11 @@ class DBStorage:
     __engine = None
     __session = None
 
+    all_class = {
+        "City": City,
+        "State": State,
+    }
+
     def __init__(self):
         """ Inicialize an instance of the class DBStorage """
 
@@ -27,30 +32,33 @@ class DBStorage:
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
-            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:/{}'.format(
+            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST,
+            HBNB_MYSQL_DB), pool_pre_ping=True)
 
         if HBNB_ENV == 'tets':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """ query session currenty of the database """
-        
+
         dicc = {}
-        if cls:
-            for instance in self.__session.query(eval(cls)).all():
-                dicc[instance.__class__.__name__ + '.' + str(instance.id)] = instance
+        if cls is None:
+            for valid_key, valid_class in DBStorage.all_class.items():
+                for instance in self.__session.query(valid_class):
+                    key = type(instance).__name__ + "." + instance.id
+                    dicc.update({key: instance})
+            return dicc
         else:
-            list_class = [User, State, City]
-            for cla in list_class:
-                for query_dic in self.__session.query(eval(cla)):
-                    dicc[query_dic.__class__.__name__ + '.' + str(query_dic.id)] = query_dic
-        return dicc
-    
+            for instance in self.__session.query(cls):
+                key = type(instance).__name__ + "." + instance.id
+                dicc.update({key: instance})
+            return dicc
+
     def new(self, obj):
         """ Add object to actually database """
         self.__session.add(obj)
-        
+
     def save(self):
         """ Commit all changes to actually database """
         self.__session.commit()
@@ -58,11 +66,11 @@ class DBStorage:
     def delete(self, obj=None):
         """ Deleted from actually database """
         self.__session.delete(obj)
-        
+
     def reload(self):
         """ load storage """
         # Create table
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
-                           expire_on_commit=False)
+                                       expire_on_commit=False)
         self.__session = scoped_session(session_factory)
