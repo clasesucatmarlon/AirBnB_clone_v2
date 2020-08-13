f1c910445952ac811c76d043651bdb5725b51697
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-13 15:06:07 -0500

    pull

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from os import getenv
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,8 +14,8 @@ class User(BaseModel, Base):
         password = Column('password' ,String(128), nullable=False)
         first_name = Column('first_name', String(128), nullable=True)
         last_name = Column('last_name', String(128), nullable=True)
-        """
         places = relationship('Place', cascade='all, delete', backref='user')
+        """
         reviews = relationship('Review', cascade='all, delete', backref='user')
         """
     else:
