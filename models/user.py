3dfd777f1934133c5a3862688225fb365a08336e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-13 19:59:15 +0000

    8. DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from os import getenv
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -11,11 +11,11 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column('email', String(128), nullable=False)
-        password = Column('password' ,String(128), nullable=False)
+        password = Column('password', String(128), nullable=False)
         first_name = Column('first_name', String(128), nullable=True)
         last_name = Column('last_name', String(128), nullable=True)
-        """
         places = relationship('Place', cascade='all, delete', backref='user')
+        """
         reviews = relationship('Review', cascade='all, delete', backref='user')
         """
     else:
