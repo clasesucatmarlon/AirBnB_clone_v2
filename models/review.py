4c8c6553096bd4a78d5c4c095b5c5aae7b2da434
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-13 18:12:56 -0500

    changes order __tablename in review

--- models/review.py
+++ models/review.py
@@ -8,8 +8,8 @@ from os import getenv
 
 class Review(BaseModel, Base):
     """This class defines a reviws atributes"""
+    __tablename__ = "reviews"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "reviews"
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
