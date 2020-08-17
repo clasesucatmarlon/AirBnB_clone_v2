0380331b7a471990f6936bb6cdbe33e04199f62d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-17 16:35:52 -0500

    script fabric that distributes an archive in two web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,19 +14,21 @@ env.hosts = ['34.74.35.201', '35.196.181.73']
 
 
 def do_pack():
-    """ Packs web_static files into .tgz file
+    """compress + bundle local sweb files
     """
-    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
-        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-    local('mkdir -p versions')
-    command = local("tar -cvzf " + file_name + " ./web_static/")
-    if command == 0:
-        return file_name
-    return None
+    try:
+        if os.path.isdir("versions") is False:
+            os.mkdir("versions")
+        time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        packed = 'versions/web_static_' + time + '.tgz'
+        fabric.api.local("tar -cvzf {} web_static".format(packed))
+        return packed
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """ deploy an archive from the archive_path
+    """deploy an archive from the archive_path
     """
     if os.path.exists(archive_path) is False:
         return False
@@ -34,15 +36,6 @@ def do_deploy(archive_path):
     target = '/data/web_static/releases/' + file_name
     path = archive_path.split('/')[1]
     try:
-        """put("{}/tmp/".format(archive_path))
-        run('sudo mkdir -p {}'.format(target))
-        run('sudo tar -xzf /tmp/{} -C {}/'.format(path, target))
-        run('sudo rm /tmp/{}'.format(path))
-        run('sudo mv {}/web_static/* {}/'.format(target, target))
-        run('sudo rm -rf {}/web_static'.format(target))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}/ /data/web_static/current'.format(target))"""
-
         put(archive_path, "/tmp/")
         run('sudo mkdir -p ' + target)
         run('sudo tar -xzf /tmp/' + path + ' -C ' + target + '/')
@@ -51,7 +44,7 @@ def do_deploy(archive_path):
         run('sudo rm -rf ' + target + '/web_static')
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s ' + target + '/ /data/web_static/current')
-
+        print('deploy success')
         return True
     except:
         return False
