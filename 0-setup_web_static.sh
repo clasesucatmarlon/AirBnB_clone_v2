8d2bc4ded1c784d46d3fd1cb41cb565933d8b52f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-17 12:54:43 -0500

    sets up web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+# Install Nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create test html file
+direct=/data/web_static/releases/test/index.html
+echo -e """
+    <html>
+        <body>
+            <h1>
+                0x03. AirBnB clone - Deploy static
+            </h1>
+        </body>
+    </html>
+""" | sudo tee $direct
+
+# Create sym link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give owner of the /data/ folder to ubuntu user and groups
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration to serve the content of /data/web_static/current/
+# to hbnb_static (https://mydomainname.tech/hbnb_static)
+config_file=/etc/nginx/sites-available/default
+sed -i '29a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $config_file
+
+# restart server
+service nginx restart
