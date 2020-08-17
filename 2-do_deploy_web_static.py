464d2aafac7702aeea06452a7829c88247ebb2f8
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-17 17:40:52 -0500

    script fabric that distributes an archive in two web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,56 @@
 #!/usr/bin/python3
-"""that distributes an archive"""
-from fabric.api import local, put, env, run
+"""
+Fabric script that distributes an archive to web servers
+"""
+from fabric.operations import put, run, sudo
+import os
+from fabric.api import run, local, sudo, env
 from datetime import datetime
-from os import path
 
-env.hosts = ['35.196.97.61', '34.75.200.40']
+
+dt = datetime.now()
+
+env.hosts = ['34.74.35.201', '35.196.181.73']
 
 
 def do_pack():
-    """contents of the web_static"""
-    f = datetime.now()
-
-    file_name = "web_static_{}{}{}{}{}{}.tgz".format(
-        f.year, f.month, f.day, f.hour, f.minute, f.second)
-
-    local('mkdir -p versions')
-    result = local('tar -cvzf versions/{} web_static'.format(file_name))
-    print("Packing web_static to versions/{}".format(file_name))
-
-    if result == 0:
-        return "versions/{}".format(file_name)
+    """ Packs web_static files into .tgz file
+    """
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    local('mkdir -p versions')ls
+    command = local("tar -cvzf " + file_name + " ./web_static/")
+    if command == 0:
+        return file_name
     return None
 
 
 def do_deploy(archive_path):
-    """Script distributes an archive"""
-    if not path.exists(archive_path):
+    """ deploy an archive from the archive_path
+    """
+    if os.path.exists(archive_path) is False:
         return False
-
+    file_name = os.path.splitext(os.path.split(archive_path)[1])[0]
+    target = '/data/web_static/releases/' + file_name
+    path = archive_path.split('/')[1]
     try:
-        archive_file = archive_path.split('/')[1]
-        file_no_ext = archive_file.split('.')[0]
-        releases = '/data/web_static/releases/versions/{}/'.format(file_no_ext)
-        put(archive_path, '/tmp')
-        run('sudo mkdir -p {}'.format(releases))
-        run('sudo tar -xvf /tmp/{} -C {}'.format(archive_file, releases))
-        run('sudo rm /tmp/{}'.format(archive_file))
-        run('sudo mv {}/web_static/* {}'.format(releases, releases))
-        run('sudo rm -rf {}/web_static'.format(releases))
+        """put("{}/tmp/".format(archive_path))
+        run('sudo mkdir -p {}'.format(target))
+        run('sudo tar -xzf /tmp/{} -C {}/'.format(path, target))
+        run('sudo rm /tmp/{}'.format(path))
+        run('sudo mv {}/web_static/* {}/'.format(target, target))
+        run('sudo rm -rf {}/web_static'.format(target))
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(releases))
+        run('sudo ln -s {}/ /data/web_static/current'.format(target))"""
+
+        put(archive_path, "/tmp/")
+        run('sudo mkdir -p ' + target)
+        run('sudo tar -xzf /tmp/' + path + ' -C ' + target + '/')
+        run('sudo rm /tmp/' + path)
+        run('sudo mv ' + target + '/web_static/* ' + target + '/')
+        run('sudo rm -rf ' + target + '/web_static')
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s ' + target + '/ /data/web_static/current')
         return True
-    except Exception as e:
+    except:
         return False
