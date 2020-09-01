5642e3312cd0707748dbf957c04fb02773e0931d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-01 15:50:13 -0500

    added and send cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-List cities
-"""
+"""script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -13,27 +11,26 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
-    """ Tear down close storage
-    """
+    """reload close files storage"""
     storage.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Display list state
-    """
-    states = storage.all(State).values()
-
-    return render_template('7-states_list.html', list=states)
+    """display a HTML page: (inside the tag BODY)"""
+    context = storage.all(State).values()
+    return render_template('7-states_list.html', states=context)
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """ Display list cities
-    """
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    return render_template('8-cities_by_states.html', list=states, Cities=cities)
+    """present in DBStorage sorted by name (A->Z)"""
+    states_complete = {
+        'states': storage.all(State).values(),
+        'cities': storage.all(City).values()
+    }
+
+    return render_template('8-cities_by_states.html', **states_complete)
 
 
 if __name__ == '__main__':
